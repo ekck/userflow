74ea744d34333d69c37cec3c0ec8144a8b23df85
kiprono <im.kiprono@gmail.com>
2023-11-26 19:51:25 +0300

    pagination and sort

--- src/main/java/com/example/userflow/controller/EmployeeController.java
+++ src/main/java/com/example/userflow/controller/EmployeeController.java
@@ -3,12 +3,12 @@ package com.example.userflow.controller;
 import com.example.userflow.entity.Employee;
 import com.example.userflow.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class EmployeeController {
@@ -19,8 +19,7 @@ public class EmployeeController {
     // display list of employees
     @GetMapping("/employees")
     public String viewHomePage(Model model) {
-        model.addAttribute("listEmployees", employeeService.getAllEmployees());
-        return "employee";
+        return findPaginated(1, "firstName", "asc", model);
     }
     @GetMapping("/showNewEmployeeForm")
     public String showNewEmployeeForm(Model model) {
@@ -33,7 +32,7 @@ public class EmployeeController {
     public String saveEmployee(@ModelAttribute("employee") Employee employee) {
         // save employee to database
         employeeService.saveEmployee(employee);
-        return "redirect:/";
+        return "redirect:/employees";
     }
 
     @GetMapping("/showFormForUpdate/{id}")
@@ -52,6 +51,29 @@ public class EmployeeController {
 
         // call delete employee method
         this.employeeService.deleteEmployeeById(id);
-        return "redirect:/";
+        return "redirect:/employees";
     }
+
+    @GetMapping("/page/{pageNo}")
+    public String findPaginated(@PathVariable(value = "pageNo") int pageNo,
+                                @RequestParam("sortField") String sortField,
+                                @RequestParam("sortDir") String sortDir,
+                                Model model) {
+        int pageSize = 5;
+
+        Page < Employee > page = employeeService.findPaginated(pageNo, pageSize, sortField, sortDir);
+        List < Employee > listEmployees = page.getContent();
+
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+
+        model.addAttribute("listEmployees", listEmployees);
+        return "employee";
+    }
+
 }
--- src/main/java/com/example/userflow/service/EmployeeService.java
+++ src/main/java/com/example/userflow/service/EmployeeService.java
@@ -1,6 +1,7 @@
 package com.example.userflow.service;
 
 import com.example.userflow.entity.Employee;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -8,7 +9,11 @@ public interface EmployeeService {
     List<Employee> getAllEmployees();
     void saveEmployee(Employee employee);
 
+    Page<Employee> findPaginated(int pageNo, int pageSize);
+
     Employee getEmployeeById(long id);
 
     void deleteEmployeeById(long id);
+
+    Page<Employee> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
 }
